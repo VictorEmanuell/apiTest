76b4b44e7303ba844e5233b90df89835928ba4de
VictorEmanuell <victoremanuelexx@gmail.com>
2021-02-11 14:53:38 -0300

    up3

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Index() {
   return (
     <>
     <h1>VictorEmanuell-Api</h1>
-    <a href="https://api.victoremanuell.tk">api.victoremanuell.tk</a>
+    <a href="https://api.victoremanuell.tk/example">https://api.victoremanuell.tk/example</a>
     </>
   )
 }
